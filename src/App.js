db4cb7ce112dfd7bebdd9fcf872ad6c1c7a39cc1
wathsala495 <sumuduwathsala80@gmail.com>
2024-09-11 16:37:33 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState,useEffect } from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const App =()=>{
+
+  const [apiId,setApiId]=useState("1");
+console.log(apiId)
+//pass callback function and dependancy array
+
+const[Data,setData]=useState({})
+const  [serach,setSearch]=useState(0)
+console.log("Data:"+ Data)
+// useEffect(()=>{
+//     console.log("useEffect running..")
+//     if(apiId.length>0){
+//       console.log("running if")
+//       const apiCall =async ()=>{
+//        const res= await  fetch(`https://jsonplaceholder.typicode.com/todos/${apiId}`)
+//        const data = await res.json()
+//        if(data){
+//         setData(data)
+//        }
+//       }
+//       apiCall()
+//     }
+    
+// },[apiId])
+
+//with button
+useEffect(()=>{
+    console.log("useEffect running..")
+    if(apiId.length>0){
+      console.log("running if")
+      const apiCall =async ()=>{
+       const res= await  fetch(`https://jsonplaceholder.typicode.com/todos/${apiId}`)
+       const data = await res.json()
+       if(data){
+        setData(data)
+       }
+      }
+      apiCall()
+      //cleanup process
+      return ()=>{
+        console.log("cleanup")
+        apiCall()
+           }
+    }
+   
+    
+},[serach])
+
+ return(
+  <div>
+    {/* <h2>{Data.title}</h2> */}
+    <input type="text" onChange={(e)=>{
+      e.preventDefault()
+      setApiId(e.target.value)}} value={apiId}/>
+      <button onClick={()=>{setSearch(pre=>pre===0?1:0)}}>Search</button>
+      {/* {Data?.map((d)=>{
+         <h3>{d.id}</h3>
+      })} */}
+      {
+        Data && (
+          <div>
+<h2>{Data.title}</h2>
+<p>{Data.completed}</p>
+
+          </div>
+        )
+      }
+
+  </div>
+ )
 }
 
 export default App;
